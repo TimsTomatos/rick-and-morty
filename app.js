bc0da881926be32a3ee71f7861b36f8f1549370d
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2018-10-04 21:10:19 -1000

    oooooof

---
+++ app.js
@@ -0,0 +1,27 @@
+var appear = document.querySelector("#button").innerHTML;
+//ar result = document.querySelector("result");
+
+
+var hi = axios.get("https://rickandmortyapi.com/api/character")
+.then(function (response) {
+    console.log(response.data.results);
+    response.data.results.forEach(function show (element) {
+        return document.querySelector("#result").innerHTML +=
+        `
+        <div class="renderMain">
+            <img class="renderPiece" src="${element.image}">
+            <div class="renderText">
+                <li>Name: ${element.name}</li>
+                <li>Species: ${element.species}</li>
+                <li>Status: ${element.status}</li>
+        </div>
+        `
+    });
+
+    // var renderedData = `<p>${hi} `
+    // results.innerHTML = renderedData;
+
+})
+
+
+
