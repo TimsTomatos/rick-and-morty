92581bc5d289c2e1ded1a2ebb1845dedecc688b5
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2018-10-17 13:12:49 -1000

    oooof

---
+++ js/app.js
@@ -0,0 +1,100 @@
+//var appear = document.querySelector("#button").innerHTML;
+var result = document.querySelector("#result");
+//const search = document.querySelector("#bar");
+//var hi = "`https://rickandmortyapi.com/api/character/?names=${search}`";
+
+
+//function getAPI (APILink) {
+
+   // let request = axios.get(hi)
+
+   function load () {
+        const text = document.querySelector("#bar").value;
+        axios.get(`https://rickandmortyapi.com/api/character/?name=${text}`)
+        .then(function (response) {
+        oof(response);
+    })
+   }
+
+   function oof (response) {
+       console.log(response.data.results);
+        result.innerHTML = response.data.results
+        .map(function (response) {
+            console.log(response.name)
+            return `
+            <div id="dataContainer">
+                <img class="renderImage" src="${response.image}">
+                <div class="renderText">
+                    <li>Name: ${response.name}</li>
+                    <li>Gender: ${response.gender}</li>
+                </div>
+            </div>
+
+            <br>
+            `;
+
+
+        })
+        .join('');
+
+
+   }
+
+
+
+
+
+
+
+
+   //Old way
+    // axios.get(`https://rickandmortyapi.com/api/character/?names=${search}`)
+    // .then(function  (response) {
+    //     response.data.results.forEach(function show (element) {
+    //         return document.querySelector("#result").innerHTML +=
+    //         `
+    //         <div class="renderMain">
+    //             <img class="renderPiece" src="${element.image}">
+    //             <div class="renderText">
+    //                 <li>Name: ${element.name}</li>
+    //                 <li>Species: ${element.species}</li>
+    //                 <li>Status: ${element.status}</li>
+    //             </div>
+    //         </div>
+
+    //         `
+    //     });
+
+    // });
+    
+
+    
+
+
+
+    //getAPI(hi);
+
+    // var renderedData = `<p>${hi} `
+    // results.innerHTML = renderedData;
+   // search.addEventListener("keypress")
+    
+
+
+
+        // var input;
+        // var filter;
+        // input =  document.querySelector("#bar").innerHTML;
+        // filter = input.value.toUpperCase();
+        // div = document.querySelector("#dropDown").innerHTML;
+        // //a = div.getElementsByTagsName("a");
+    
+    
+    
+    
+    
+//})
+
+
+
+
+
